d0795523d3e4cf6dca656cd6856ff3fd7dead3f6
Boosterloop <alison.savary@me.com>
2020-01-14 18:11:50 +0100

    Loading data in DB

--- microservices/citylog/src/main/java/ch/heigvd/amt/citylog/api/endpoints/CountriesApiController.java
+++ microservices/citylog/src/main/java/ch/heigvd/amt/citylog/api/endpoints/CountriesApiController.java
@@ -29,12 +29,14 @@ public class CountriesApiController implements CountriesApi {
     private CountryEntity toCountryEntity(Country country) {
         CountryEntity entity = new CountryEntity();
         entity.setName(country.getName());
+        entity.setCountryCode(country.getCountryCode());
         return entity;
     }
 
     private Country toCountry(CountryEntity entity) {
         Country country = new Country();
         country.setName(entity.getName());
+        country.setCountryCode(entity.getCountryCode());
         return country;
     }
 }
