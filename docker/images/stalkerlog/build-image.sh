63fdd994876b63e7668c5537775724e7061da4c5
Laykel <layke.luc@gmail.com>
2020-01-15 09:12:53 +0100

    Create build script for main api

---
+++ docker/images/stalkerlog/build-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir tmp
+
+mvn clean package -f ../../../microservices/stalkerlog/pom.xml
+cp ../../../microservices/stalkerlog/target/stalkerlog*.jar ./tmp/
+
+docker build -t stalkerlog/api .
